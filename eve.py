7038b6f1c36e3461e719f92b4796824c2fd80313
Zach Howard <zdhoward@icloud.ca>
2018-01-13 01:04:43 -0500

    fixing price

--- eve.py
+++ eve.py
@@ -71,30 +71,12 @@ def getPrices(itemName):
         for item in itemIDs:
             if (each['type_id'] != None) & (each['type_id'] != 0) & (each['type_id'] != '0'):
                 if each['type_id'] == int(item):
-                    #msg += item
-                    #msg += '> '
                     msg += eveToName(item)
                     msg += '> avg: '
                     msg += '{:,}'.format(each['average_price'])
                     msg += '\n'
                     #msg += ' adj: '
                     #msg += '{:,}'.format(each['adjusted_price'])
-                    #msg += 'DEBUG:\n'
-                    #msg += str(each)
-
-                    #
-                    #Ignoring exception in on_message
-                    #Traceback (most recent call last):
-                    #  File "/home/zecx/.local/lib/python3.5/site-packages/discord/client.py", line 307, in _run_event
-                    #    yield from getattr(self, event)(*args, **kwargs)
-                    # File "dankbot.py", line 222, in on_message
-                    #    msg += getPrice(item)
-                    # File "/home/zecx/Scripts/dankbot/eve.py", line 70, in getPrice
-                    #    msg += '{:,}'.format(each['average_price'])
-                    #KeyError: 'average_price'
-                    #
-
     return msg
 
 #pprint(getPrices("Antimatter"))
-#proc = subprocess.Popen(['sudo','/usr/sbin/apache2ctl','restart'])
